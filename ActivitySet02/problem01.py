1bca966d404bf7c51d5b304ed3d0ffeb9e78065c
Gayathri K <95470777+rithvikteja@users.noreply.github.com>
2022-08-07 17:57:48 +0000

    crush

--- ActivitySet02/problem01.py
+++ ActivitySet02/problem01.py
@@ -1,8 +1,8 @@
-import calendar
-year=2022
-month=7
-x=calendar.month(year,month)
-print(x)
+#import calendar
+#year=2022
+#month=7
+#x=calendar.month(year,month)
+#print(x)
 
 #def add(a, b):
     #return  # ...
@@ -14,3 +14,38 @@ print(x)
 
     #c = add(a, b)
     #print  # ...
+#from turtle import*
+#import colorsys
+
+#speed(0)
+#pensize(3)
+#bgcolor('black')
+#hue=0.0
+
+#for i in range(300):
+  #col=colorsys.hsv_to_rgb(hue,1,1)
+  #pencolor(col)
+  #hue+=0.005
+  #circle(5-i,100)
+  ##lt(80)
+  #circle(5-i,100)
+  #rt(100)
+#done()  
+#import pywhatkit as kit
+
+#number='6361284487'
+#kit.sendwhatmsg(
+  #number,
+  #'Happy Birthday!',0,0)
+#from turtle import*
+#color('purple')
+#begin_fill()
+#pensize(4)
+#left(50)
+#forward(133)
+#circle(50,200)
+#right(140)
+#circle(50,200)
+#forward(133)
+#end_fill()
+
